40b4b64b64f6346624231255ca87c498b501abad
Eban-Covarrubias <130101682+Eban-Covarrubias@users.noreply.github.com>
2024-07-29 16:47:06 -0700

    Updated assembler to recognize NOP and BOF operations, finished program 3 (16 bit multiplication), and generated machine code for program 3. Also cleaned up and removed outdated files

--- notesOnPrograms/program3.c
+++ notesOnPrograms/program3.c
@@ -2,4 +2,43 @@
 //Each mem slot holds 8 bits
 //Start by multipying LSBs
 
-LowRes = Alo*Blo
+for(i = 0; i < 64; i+= 4){
+    //note, mem[i] = Ahi, mem[i+1] = Alo
+    // mem[i+2] = Bhi, mem[i+3] = Blo
+    mem[i+64] = 0;
+    mem[i+65] = 0;
+    mem[i+66] = 0;
+    mem[i+67] = 0;
+
+
+    resltLowlo1, carry = Low_mult(mem[i+1],mem[i+3]); //loop by 8 bit subroutine 1
+    mem[i+67] = resltLowhi1 //First quarter done
+
+    mem[i+66] = carry
+    mem[i+66] += High_mult(mem[i+1], mem[i+3]); //loop by 8 bit subroutine 2
+
+
+    mem[i+66] += Low_mult(mem[i],mem[i+3]); //loop by 8 bit subroutine 3
+    carry = Low_mult_carry(mem[i],mem[i+3]); 
+
+    mem[i+65] = carry;
+    mem[i+65] += High_mult(mem[i], mem[i+3]); //loop by 8 bit subroutine 4
+    carry = High_mult_carry(mem[i], mem[i+3]);
+    mem[i+64] = carry
+
+    mem[i+66] += Low_mult(mem[i+1],mem[i+2]); //loop by 8 bit subroutine 5
+    carry = Low_mult_carry(mem[i+1],mem[i+2]); //Done with second quarter
+    mem[i+65] += carry //add any carry bits
+
+    mem[i+65] += High_mult(mem[i+1], mem[i+2]); //loop by 8 bit subroutine 6
+    carry = High_mult_carry(mem[i+1], mem[i+2]);
+    mem[i+64] += carry;
+
+
+    mem[i+65] += Low_mult(mem[i],mem[i+2]); //loop by 8 bit subroutine 7 
+    carry =  Low_mult_carry(mem[i],mem[i+2]); //Done with third quarter 8
+
+    mem[i+64] += carry;
+    mem[i+64] += High_mult(mem[i], mem[i+2]); //loop by 8 bit subroutine
+}
+//let mem[128]=carry, mem[129]=i, mem[130]=j, mem[131]=shiftingOpA, mem[132]=shiftingOpB
